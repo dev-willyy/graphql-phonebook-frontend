7735f9cd192d746a78a444053a4011b6346c72b1
Madu Williams <97499846+Prodigy97@users.noreply.github.com>
2023-05-27 04:07:34 +0100

    Finished the lecture module for graphQL part B

---
+++ src/App.js
@@ -0,0 +1,41 @@
+// pass {pollIntervals: 2000} as a second option into the useQuery() hook
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import Persons from "./components/Persons";
+import PersonForm from "./components/PersonForm";
+import PhoneForm from "./components/PhoneForm";
+import { ALL_PERSONS } from "./queries";
+
+const Notify = ({ errorMessage }) => {
+    if (!errorMessage) {
+        return null;
+    }
+    return <div style={{ color: "red" }}>{errorMessage}</div>;
+};
+
+const App = () => {
+    const [errorMessage, setErrorMessage] = useState(null);
+    const result = useQuery(ALL_PERSONS);
+
+    if (result.loading) {
+        return <div>loading...</div>;
+    }
+
+    const notify = (message) => {
+        setErrorMessage(message);
+        setTimeout(() => {
+            setErrorMessage(null);
+        }, 10000);
+    };
+
+    return (
+        <div>
+            <Notify errorMessage={errorMessage} />
+            <Persons persons={result.data.allPersons} />
+            <PersonForm setError={notify} />
+            <PhoneForm setError={notify} />
+        </div>
+    );
+};
+
+export default App;
